9dc459ab64ad13b38f1e6bc613525f2353a3f3ad
jakubdrewniak <drewniak.jakub@gmail.com>
2018-11-12 19:26:13 +0100

    random tiles adding implemented

--- 2048.js
+++ 2048.js
@@ -1,12 +1,36 @@
 function Game() {
     this.gameBoard = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
+    this.tilePosition = {x: 0, y:0}
     this.init()
+
 }
 
 Game.prototype.init = function () {
-    this.makeGameBoard()
-    this.makeEmptyGameBoardArray()
-    this.placeTiles()
-    this.render()
-    this.startListeningArrowKeys()
+    this.getNewTilePosition()
+    this.placeTileOnBoard()
+    this.getNewTilePosition()
+    this.placeTileOnBoard()
+    console.log(this.gameBoard)
+}
+
+Game.prototype.getNewTilePosition = function () {
+    const newTilePosition = {
+        x: this.getRandomInt(0, 3),
+        y: this.getRandomInt(0, 3)
+    }
+    if (this.gameBoard.join().split(",").filter(x=> x==="0").length === 0) {this.endGame()
+    } else if (this.gameBoard[newTilePosition.y][newTilePosition.x] !== 0) {this.getNewTilePosition()
+    } else { this.tilePosition = newTilePosition }    
+}
+
+Game.prototype.placeTileOnBoard = function () {
+    this.gameBoard[this.tilePosition.y][this.tilePosition.x] = 2
+}
+
+Game.prototype.getRandomInt = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+Game.prototype.endGame = function () {
+    console.log("Game Over!")
 }
