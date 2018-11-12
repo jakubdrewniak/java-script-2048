bfbf594a83380955082579c9f352f6d9e0ec573b
jakubdrewniak <drewniak.jakub@gmail.com>
2018-11-12 18:19:24 +0100

    css file added. created game board

--- 2048.js
+++ 2048.js
@@ -1,2 +1,12 @@
 function Game() {
+    this.gameBoard = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
+    this.init()
+}
+
+Game.prototype.init = function () {
+    this.makeGameBoard()
+    this.makeEmptyGameBoardArray()
+    this.placeTiles()
+    this.render()
+    this.startListeningArrowKeys()
 }
